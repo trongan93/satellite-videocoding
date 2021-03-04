00f82c0877f5cf7e83b8b2d6b0525df8053fed39
trongan93 <trongan93@gmail.com>
2021-03-04 12:03:24 +0800

    PSNR calculation by using ffmpeg (default mode)

--- h264_encoding.sh
+++ h264_encoding.sh
@@ -9,6 +9,4 @@ video_output_path="./video-sequences/${video_name}_encoded.mp4"
 ffmpeg -y -f rawvideo -vcodec rawvideo -pix_fmt gray -s 2560x2560 -r 1 -i ${video_input_path} -codec:v libx264 -c:a copy ${video_output_path}
 
 # Check the psnr
-#ffmpeg -i ${video_input_path} -i ${video_output_path} -filter_complex "psnr" -f null /dev/null
-#ffmpeg -i ${video_output_path} -i ${video_input_path} "[0:v]scale=2560x2560,format=pix_fmts=gray,fps=fps=1; [1:v]scale=2560x2560,format=pix_fmts=gray,fps=fps=1; [1v][0v]psnr" -f null /dev/null
 ffmpeg -i ${video_output_path} -s 2560x2560 -pix_fmt gray -r 1 -i ${video_input_path} -s 2560x2560 -pix_fmt gray -r 1 -filter_complex "psnr" -f null /dev/null

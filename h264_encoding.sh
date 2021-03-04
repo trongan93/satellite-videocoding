5306c88e8db6f8fcf0ac4c23497a2c699bc67093
trongan93 <trongan93@gmail.com>
2021-03-04 11:20:05 +0800

    h264 encoding by using ffmpeg (default mode)

--- h264_encoding.sh
+++ h264_encoding.sh
@@ -3,6 +3,7 @@ video_input_path="/home/trongan93/Projects/github/satellite-videocoding/video-se
 echo ${video_input_path}
 video_output_path="./video-sequences/${video_name}_encoded.mp4"
 
-#encoding
-#Ref at https://forum.videohelp.com/threads/397557-ffmpeg-luma-only
+# encoding
+# Ref at https://forum.videohelp.com/threads/397557-ffmpeg-luma-only
+# The default mode of H.264
 ffmpeg -y -f rawvideo -vcodec rawvideo -pix_fmt gray -s 2560x2560 -r 1 -i ${video_input_path} -codec:v libx264 -c:a copy ${video_output_path}

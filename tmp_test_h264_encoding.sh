092a8969d59681612046f89e544284c7ecc905d7
trongan93 <trongan93@gmail.com>
2021-03-11 11:03:56 +0800

    add egsg evaluation results

--- tmp_test_h264_encoding.sh
+++ tmp_test_h264_encoding.sh
@@ -18,6 +18,6 @@ video_output_path="./video-sequences/${video_name}_encoded_tmp.mp4"
 #keyint specifies the maximum length of the GOP, so the maximum interval between each keyframe, which remember that can be either an IDR frame or a non-IDR frame. I'm not completely sure but I think that by default ffmpeg will require every I-frame to be an IDR frame, so in practice you can use the terms IDR frame and I-frame interchangeably
 #min-keyint specifies the minimum length of the GOP. This is because the encoder might decide that it makes sense to add a keyframe before the keyint value, so you can put a limit
 #no-scenecut. When the encoder determines that there's been a scene cut, it may decide to insert an additional I-frame. The issue is that I-frames are very expensive if compared to other frame types, so when encoding for streaming you want to disable it.
-ffmpeg -y -f rawvideo -vcodec rawvideo -pix_fmt gray -s 2560x2560 -r 1 -i ${video_input_path} -codec:v libx264 -x264-params keyint=1:min-keyint=1:scenecut=0:bframes=0:b-adapt=0 -qp 34 -c:a copy ${video_output_path}
+ffmpeg -y -f rawvideo -vcodec rawvideo -pix_fmt gray -s 2560x2560 -r 1 -i ${video_input_path} -codec:v libx264 -x264-params keyint=12:min-keyint=1:scenecut=0:bframes=0:b-adapt=0 -qp 32 -c:a copy ${video_output_path}
 # Check the PSNR
 ffmpeg -i ${video_output_path} -s 2560x2560 -pix_fmt gray -r 1 -i ${video_input_path} -s 2560x2560 -pix_fmt gray -r 1 -filter_complex "psnr" -f null /dev/null

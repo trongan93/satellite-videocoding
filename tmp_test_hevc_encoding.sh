389936da92aef906df02bd1248f432c724be55d2
trongan93 <trongan93@gmail.com>
2021-03-15 15:06:44 +0800

    update experiment results

--- tmp_test_hevc_encoding.sh
+++ tmp_test_hevc_encoding.sh
@@ -1,4 +1,4 @@
-video_name="taipei_260_271"
+video_name="songshan_airport"
 video_input_path="/home/trongan93/Projects/github/satellite-videocoding/video-sequences/${video_name}.yuv"
 echo ${video_input_path}
 video_output_path="./video-sequences/${video_name}_encoded_tmp_hevc.mp4"
@@ -11,6 +11,6 @@ video_output_path="./video-sequences/${video_name}_encoded_tmp_hevc.mp4"
 #keyint specifies the maximum length of the GOP, so the maximum interval between each keyframe, which remember that can be either an IDR frame or a non-IDR frame. I'm not completely sure but I think that by default ffmpeg will require every I-frame to be an IDR frame, so in practice you can use the terms IDR frame and I-frame interchangeably
 #min-keyint specifies the minimum length of the GOP. This is because the encoder might decide that it makes sense to add a keyframe before the keyint value, so you can put a limit
 #no-scenecut. When the encoder determines that there's been a scene cut, it may decide to insert an additional I-frame. The issue is that I-frames are very expensive if compared to other frame types, so when encoding for streaming you want to disable it.
-ffmpeg -y -f rawvideo -vcodec rawvideo -pix_fmt gray -s 2560x2560 -r 1 -i ${video_input_path} -codec:v libx265 -x265-params "keyint=12:min-keyint=1:scenecut=0:bframes=0:b-adapt=0:qp=32" -c:a copy ${video_output_path}
+ffmpeg -y -f rawvideo -vcodec rawvideo -pix_fmt gray -s 2560x2560 -r 1 -i ${video_input_path} -codec:v libx265 -x265-params "keyint=1:min-keyint=1:scenecut=0:bframes=0:b-adapt=0:qp=32" -c:a copy ${video_output_path}
 # Check the PSNR
 ffmpeg -i ${video_output_path} -s 2560x2560 -pix_fmt gray -r 1 -i ${video_input_path} -s 2560x2560 -pix_fmt gray -r 1 -filter_complex "psnr" -f null /dev/null
